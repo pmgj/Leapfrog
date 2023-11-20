076672cdab9c39fd08928097e89470d271c2284f
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2023-11-20 20:17:47 -0300

    kjghgkjg

--- src/main/java/model/Test.java
+++ src/main/java/model/Test.java
@@ -1,16 +1,23 @@
 package model;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
-import model.boards.Board;
-import model.boards.TraditionalBoard;
+import model.games.Traditional;
 
 public class Test {
     public static void main(String[] args) {
-        Board b = new TraditionalBoard();
-        CellState[][] board = b.createBoard(4, 5);
+        LeapFrog game = new Traditional();
+        game.initialize(6, 6);
+        Scanner sc = new Scanner(System.in);
+        CellState[][] board = game.getBoard();
         for (CellState[] cellStates : board) {
             System.out.println(Arrays.toString(cellStates));
         }
+        System.out.print("Selecionar coordenadas da peça: ");
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        System.out.println(row);
+        System.out.println(col);
     }
 }
--- src/main/java/model/games/Murray.java
+++ src/main/java/model/games/Murray.java
@@ -9,6 +9,7 @@ public class Murray extends LeapFrog {
     public Murray() {
         super("Murray");
     }
+
     public void initialize(int rows, int cols) {
         super.initialize(rows, cols);
         this.board = new MurrayBoard().createBoard(rows, cols);
