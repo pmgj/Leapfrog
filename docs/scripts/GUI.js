007bb86768b316035ae277eaeea683a33300f10a
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2023-12-12 09:57:28 -0300

    GUI private members

--- docs/scripts/GUI.js
+++ docs/scripts/GUI.js
@@ -5,37 +5,40 @@ import Murray from "./games/Murray.js"
 import Traditional from "./games/Traditional.js"
 
 class GUI {
+    #game
+    #path
+    #rules
     constructor() {
-        this.game = null;
-        this.path = [];
-        this.rules = [new Murray(), new Traditional()];
+        this.#game = null;
+        this.#path = [];
+        this.#rules = [new Murray(), new Traditional()];
     }
-    start() {
+    #start() {
         let iSize = document.getElementById("size");
         let size = iSize.valueAsNumber;
         let option = document.querySelector("#rules").selectedIndex;
-        this.game = this.rules[option];
-        this.game.initialize(size, size);
-        let board = this.game.getBoard();
-        this.printBoard(board);
-        this.changeMessage();
-        this.updateScore();
+        this.#game = this.#rules[option];
+        this.#game.initialize(size, size);
+        let board = this.#game.getBoard();
+        this.#printBoard(board);
+        this.#changeMessage();
+        this.#updateScore();
     }
     init() {
         let rules = document.querySelector("#rules");
-        for (let r of this.rules) {
+        for (let r of this.#rules) {
             let option = document.createElement("option");
             option.text = r;
             rules.add(option);
         }
         let iSize = document.getElementById("size");
         let iStart = document.getElementById("start");
-        rules.onchange = this.start.bind(this);
-        iSize.onchange = this.start.bind(this);
-        iStart.onclick = this.start.bind(this);
-        this.start();
+        rules.onchange = this.#start.bind(this);
+        iSize.onchange = this.#start.bind(this);
+        iStart.onclick = this.#start.bind(this);
+        this.#start();
     }
-    printBoard(board) {
+    #printBoard(board) {
         let tbody = document.querySelector("#board tbody");
         tbody.innerHTML = "";
         for (let i = 0; i < board.length; i++) {
@@ -48,26 +51,26 @@ class GUI {
                     img.src = `images/${board[i][j]}.svg`;
                     td.appendChild(img);
                 }
-                td.onclick = this.play.bind(this);
+                td.onclick = this.#play.bind(this);
                 tr.appendChild(td);
             }
         }
     }
-    play(evt) {
+    #play(evt) {
         let td = evt.currentTarget;
         td.className = "selected";
-        let cell = this.coordinates(td);
-        if (this.path.length > 0 && this.path[this.path.length - 1].equals(cell)) {
-            this.endPath();
+        let cell = this.#coordinates(td);
+        if (this.#path.length > 0 && this.#path[this.#path.length - 1].equals(cell)) {
+            this.#endPath();
         } else {
-            this.path.push(cell);
+            this.#path.push(cell);
         }
     }
-    async endPath() {
+    async #endPath() {
         try {
-            let mr = this.game.move(this.path);
+            let mr = this.#game.move(this.#path);
             const time = 1000;
-            let temp = this.path;
+            let temp = this.#path;
             for (let i = 1; i < temp.length; i++) {
                 let { x: or, y: oc } = temp[i - 1];
                 let { x: dr, y: dc } = temp[i];
@@ -77,7 +80,7 @@ class GUI {
                     anim.onfinish = () => middleImage.parentNode.removeChild(middleImage);
                     let image = document.querySelector(`tr:nth-child(${or + 1}) td:nth-child(${oc + 1}) img`);
                     let moveImage = () => {
-                        this.getTableData(temp[i]).appendChild(image);
+                        this.#getTableData(temp[i]).appendChild(image);
                         resolve(true);
                     };
                     let td = document.querySelector("td");
@@ -86,38 +89,38 @@ class GUI {
                     anim2.onfinish = moveImage;
                 });
             }
-            this.changeMessage(mr);
-            this.updateScore();
+            this.#changeMessage(mr);
+            this.#updateScore();
         } catch (ex) {
-            this.setMessage(ex.message);
+            this.#setMessage(ex.message);
         }
-        this.path = [];
+        this.#path = [];
         document.querySelectorAll(".selected").forEach(td => td.className = "");
     }
-    updateScore() {
+    #updateScore() {
         let td = document.querySelectorAll("#score tbody td");
-        let scores = this.game.getScores();
+        let scores = this.#game.getScores();
         td[0].textContent = scores.get(Player.PLAYER1);
         td[1].textContent = scores.get(Player.PLAYER2);
     }
-    coordinates(cell) {
+    #coordinates(cell) {
         return new Cell(cell.parentNode.rowIndex, cell.cellIndex);
     }
-    getTableData({ x, y }) {
+    #getTableData({ x, y }) {
         let table = document.querySelector("#board");
         return table.rows[x].cells[y];
     }
-    changeMessage(m) {
+    #changeMessage(m) {
         let objs = { PLAYER2: "Player 2 win!", PLAYER1: "Player 1 win!", DRAW: "Draw." };
         if (objs[m]) {
-            this.setMessage(`Game Over! ${objs[m]}`);
+            this.#setMessage(`Game Over! ${objs[m]}`);
             document.querySelectorAll("#board td").forEach(td => td.onclick = undefined);
         } else {
             let msgs = { PLAYER1: "Player 1 turn.", PLAYER2: "Player 2 turn." };
-            this.setMessage(msgs[this.game.getTurn()]);
+            this.#setMessage(msgs[this.#game.getTurn()]);
         }
     }
-    setMessage(message) {
+    #setMessage(message) {
         let msg = document.getElementById("message");
         msg.textContent = message;
     }
